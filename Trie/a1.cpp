bff41cfba5d2140980018a9433126cf4c306e3cc
rogerBen <ben.tfpm@gmail.com>
2017-08-25 12:48:23 +1000

    Added insertion sort on wordCount

--- Trie/a1.cpp
+++ Trie/a1.cpp
@@ -21,36 +21,36 @@ struct trieNode {
 	int words;
 	int prefixCount;	//How many children the node has
 	struct trieNode *child[26];
-	bool isEnd;
+	int isEnd;	//-1 means not end, 0 or greater means yes
 	char letter;		//The character in the trieNode
 }*head;
 
 
 
 void parseLine(string);
-
-
 string lower(string);
 void initialize();
 void addWord(string);
-int countPreffixes();
-int countWords();
 bool search(string);
 int wordsWithPrefix(string);
 string checkWord(string);
+void sortList(int[], int, int);
+void printArray(int[], int);
+int partition(int[], int, int);
+void quickSort(int[], int, int);
+void swap(int*, int*);
+void insertionSort(int[], int);
 
 
-
-
-int wordCount = 0;
-string words[MAXWORDS];
+int wordCount = 0;	//Count of total words
+int uniqueCount = 0; //Count of unique words
+string words[MAXWORDS]; //Stores unique words
 
 int main(int argc, char* argv[]) {
 
 	fstream fin;
 	initialize();
 	fin.open(argv[1]);
-
 
 	if(fin.good()) {
 		while(!fin.eof()) {
@@ -61,21 +61,42 @@ int main(int argc, char* argv[]) {
 			parseLine(tempLine); //parseLine calls checkWord and addWord
 
 		}
-
 		//Document has been read 
 		cout << "Word count: " << wordCount << endl;
+		int wordsCount[uniqueCount];
+		cout << "Unique word count: " << uniqueCount << endl;
+		
+		for(int i = 0; i < uniqueCount; i++) {
+			//Creates new array of word counts corresponding with indexes in words
+			//wordsCount[i] is the count for words[i]
+			wordsCount[i] = wordsWithPrefix(words[i]);
+
+			//Output list for testing
+			//cout << words[i] << " " << wordsCount[i] << endl;
+		}
+		
+		//Sort by decreasing count
+		//sortList(wordsCount, 0, uniqueCount);
+		//quickSort(wordsCount, 0, uniqueCount);
+		insertionSort(wordsCount, uniqueCount);
+		printArray(wordsCount, uniqueCount);
 
 
-			if(search("bite")) {
-				cout << "Found bite" << endl;
-			}
-			if(search("mimsy")) {
-				cout << "Found mimsy" << endl;
-			}
 
-			cout << "Number of words with prefix ban are " << wordsWithPrefix("ban") << endl;
 
-				cout << "brillig count: " << wordsWithPrefix("brillig") << endl;
+		//Output first ten words in sorted list with count
+		
+		cout << endl << "****Top 10****" << endl;
+		for(int i = 0; i <= 10; i ++) {
+			cout << words[i] << " " << wordsCount[i] << endl;
+		}
+
+
+		//Output last ten words in list along with counts
+		cout << endl << "****Bottom 10****" << endl;
+		for(int j = uniqueCount-1; j > (uniqueCount-11); j--) {
+			cout << words[j] << " " << wordsCount[j] << endl;
+		}
 			
 	}else {
 		cerr << "ERROR OPENING FILE..." << endl;
@@ -123,7 +144,7 @@ string checkWord(string word) {
 			//DO nothing
 		}
 	}
-	wordCount++;
+	//wordCount++;
 	return newWord;
 }
 
@@ -138,25 +159,21 @@ string lower(string word) {
 		lowerChar = tolower(lowerChar);
 		newWord = newWord+lowerChar; 
 	}
-	//words[wordCount] = newWord;
-	wordCount++;
+	
 	return newWord;
 }
 
 //First have to initialize the root element to 0
 void initialize() {
 	head = new trieNode();
-	head->isEnd = false;
+	head->isEnd = -1;
 	head->prefixCount = 0;
 
 }
 
-
 //When a word is added to a node we will add word to the corresponding
 //branch of node cutting the leftmost character of word
 //If the needed branch does not exist we will have to create it
-
-
 void addWord(string word) {
 	
 	//Create node and set to root
@@ -181,12 +198,15 @@ void addWord(string word) {
 		current = current->child[letter];
 	}
 	//End of word
-	current->isEnd = true;
-	
+	current->isEnd = 0;
+	wordCount++;
+
 	if(wordsWithPrefix(word) > 1) {
 
 	}else {
-		cout << "Unique: " << word << endl;
+		//Word is unique so store it and increase unique word count
+		words[uniqueCount] = word;
+		uniqueCount++;
 	}
 }
 
@@ -209,7 +229,7 @@ bool search(string word) {
 		current = current->child[letter];
 	}
 	//If word is in trie this will return true
-	return current->isEnd;
+	return current->isEnd = 0;
 }
 
 
@@ -235,4 +255,97 @@ int wordsWithPrefix(string prefix) {
 	return current->prefixCount;
 }
 
+/*
+void sortList(int wordsCount[], int left, int right) {
+
+	//Perform quicksort on list
+	int i = left;
+	int j = right-1;
+	int tmp;
+	
+	//Choose a pivot value
+
+	int pivotIndex = (j-i)/2;
+	cout << i << " " << j << " " << pivotIndex << endl;
+	int pivot = wordsCount[(j-i)/2]; //Correct
+	cout << "Pivot: " << pivot << endl;
+
+	while(i <= j) {
+
+		cout << "Before i: " << i << " wordsCount[i]: " << wordsCount[i] << " j: " << j << " wordsCount[j]: " << wordsCount[j] << endl;
+		
+		//Move i forward until an element with value greater or equal to pivot is found
+		while(wordsCount[i] < pivot) {
+			i++; //should skip this first turn
+		}
+		
+		//Move j back until an element with value lesser or equal to pivot
+		while(wordsCount[j] > pivot) {
+			j--;
+		}
+		
+		//If i <= j then they are swapped and i steps to the next position
+		if(wordsCount[i] == wordsCount[j]) {
+			i++;
+		}else if (i < j) {
+			tmp = wordsCount[i];
+			wordsCount[i] = wordsCount[j];
+			wordsCount[j] = tmp;
+			i++;
+			j--;
+		}
+		cout << "After i: " << i << " wordsCount[i]: " << wordsCount[i] << " j: " << j << " wordsCount[j]: " << wordsCount[j] << endl;
+		
+		int temp;
+		cin >> temp;
+	}
+
+	//Recursion
+	if(left < j) {
+		//cout << "hello";
+		sortList(wordsCount, left, j);
+	}
+	if(i < right) {
+		//cout << "bfbf";
+		sortList(wordsCount, i, right);
+	}		
+}
+*/
+
+void swap(int *a, int *b) {
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+
+void printArray(int wordsCount[], int size) {
+	cout << "Sorted list" << endl;
+	for(int i =0; i < size; i++) {
+		cout << words[i] << ": " << wordsCount[i] << endl;
+	}
+}
+
+void insertionSort(int wordsCount[], int length) {
+	int j, temp;
+
+	for(int i=0; i < length; i++) {
+		j = i;
+		//While there are still elements in the list
+		while(j > 0 && wordsCount[j] > wordsCount[j-1]) {
+			if(wordsCount[j] == wordsCount[j-1]) {
+				//Same count, sort alphabetically
+				if(words[j].compare(words[j-1]) > 1) {
+					cout << words[j] << " " << words[j-1] << endl;
+				}
+			}else {
+				swap(wordsCount[j], wordsCount[j-1]);
+				swap(words[j], words[j-1]); //To maintain relationship of indexes
+				j--;
+			}
+
+		}
+			
+	}
+}
 
